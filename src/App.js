3b131e234f2ba88320c48cb7e65478f5a0ab9d12
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2023-07-30 21:10:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Bmi_Calci from './Components/Bmi_Calci.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Bmi_Calci/>
+    </div>
+  );
+}
+
+export default App;
